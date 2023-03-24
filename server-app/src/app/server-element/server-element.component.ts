c39b115bfbf49ecb19ae6a5951070303915c1595
sindabad764 <sindabad764@outlook.com>
2023-03-24 11:16:30 -0700

    bind to custom properties

--- server-app/src/app/server-element/server-element.component.ts
+++ server-app/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,14 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+  //@Input makes the property bindable from outside the component
+  @Input() element:{type: string, name: string, content:string};
+
+  constructor(){}
+  
+
 }
