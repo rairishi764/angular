5c9bc1fa34ce5c214226fab09285c94e77b696fb
sindabad764 <sindabad764@outlook.com>
2023-03-24 00:24:30 -0700

    shopping list and ingredients set

--- first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes:Recipe[] = [
-    new Recipe('A','A-desc','a-url')
+    new Recipe('A','A-desc','https://images.immediate.co.uk/production/volatile/sites/30/2022/08/Chicken-Tikka-99647a6.jpg?quality=90&webp=true&resize=600,545')
   ];
   constructor(){}
 
---
+++ first-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,7 @@
+export class Ingredient{
+    //public name:string;
+    //public amount:number;
+
+    //shortcut to create vars directly through constructor
+    constructor(public name:string, public amount:number){}
+}
