cc5b4e94f6f3f772837d6e3758e9672943147162
sindabad764 <sindabad764@outlook.com>
2023-03-24 12:55:11 -0700

    remove view encapulation

--- server-app/src/app/server-element/server-element.component.ts
+++ server-app/src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  //encapsulation: ViewEncapsulation.None // removes component level segregation of styles throughout project
 })
 export class ServerElementComponent {
   //@Input makes the property bindable from outside the component
