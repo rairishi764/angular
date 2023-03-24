a0a6982043c98a107ea5bb61302c068e507e9898
sindabad764 <sindabad764@outlook.com>
2023-03-24 11:20:25 -0700

    alias to custom property

--- server-app/src/app/server-element/server-element.component.ts
+++ server-app/src/app/server-element/server-element.component.ts
@@ -7,9 +7,13 @@ import { Component, Input } from '@angular/core';
 })
 export class ServerElementComponent {
   //@Input makes the property bindable from outside the component
-  @Input() element:{type: string, name: string, content:string};
+  //@Input() element:{type: string, name: string, content:string};
+
+  //Assigning alias to the custom element to be used for binding in parent component
+  //used to bind in app.component
+  @Input('srvElement') element:{type: string, name: string, content:string};
 
   constructor(){}
-  
+
 
 }
